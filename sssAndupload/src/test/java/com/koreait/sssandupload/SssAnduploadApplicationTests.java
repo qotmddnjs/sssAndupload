3409395a92465428c13a6603d741e37703b5c0ae
qotmddnjs <dudcnsaus@gmail.com>
2024-05-22 14:14:43 +0900

    테스트 환경의 샘플데이터 생성, 테스트 DB 분리

--- sssAndupload/src/test/java/com/koreait/sssandupload/SssAnduploadApplicationTests.java
+++ sssAndupload/src/test/java/com/koreait/sssandupload/SssAnduploadApplicationTests.java
@@ -1,13 +1,61 @@
 package com.koreait.sssandupload;
 
+import com.koreait.exam.sssandupload.app.home.controller.HomeController;
+import com.koreait.exam.sssandupload.app.home.controller.HomeController;
+import com.koreait.exam.sssandupload.app.member.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-class SssAnduploadApplicationTests {
+@AutoConfigureMockMvc
+@Transactional
+@ActiveProfiles({"base-addi", "test"})
+class AppTests {
+
+  @Autowired
+  private MockMvc mvc;
+
+  @Autowired
+  private MemberService memberService;
 
   @Test
-  void contextLoads() {
+  @DisplayName("메인화면에서는 안녕이 나와야 함")
+  void t1() throws Exception {
+    // WHEN
+    // GET /
+    ResultActions resultActions = mvc
+        .perform(get("/"))
+        .andDo(print());
+
+    // THEN
+    // 안녕
+    resultActions
+        .andExpect(status().is2xxSuccessful())
+        .andExpect(handler().handlerType(HomeController.class))
+        .andExpect(handler().methodName("main"))
+        .andExpect(content().string(containsString("안녕")));
   }
 
+  @Test
+  @DisplayName("회원의 수")
+  @Rollback(false)
+  void t2() throws Exception {
+    long count = memberService.count();
+    assertThat(count).isGreaterThan(0);
+  }
 }
